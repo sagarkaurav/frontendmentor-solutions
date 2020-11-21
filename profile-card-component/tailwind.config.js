854f3a8018792adcac09ea720557f8c9be61e8ad
Sagar Kaurav <sagar.kaurav@gmail.com>
2020-11-21 08:16:45 +0000

    profile card component solution

---
+++ profile-card-component/tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        cyan: colors.cyan
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

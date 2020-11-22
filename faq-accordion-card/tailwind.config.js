6daea6bd5835a0bcb81417b07694e34a31280986
Sagar Kaurav <sagar.kaurav@gmail.com>
2020-11-22 07:42:51 +0000

    add faq-accordian-card solution

---
+++ faq-accordion-card/tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV == "production",
+    content: [
+      './public/index.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        violet: colors.violet
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+}

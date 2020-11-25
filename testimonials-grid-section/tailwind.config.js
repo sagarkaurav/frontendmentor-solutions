7eeec10c67e641ed36ad725a83162807d9c51d38
Sagar Kaurav <sagar.kaurav@gmail.com>
2020-11-25 05:52:00 +0000

    testimonials-grid-section solution

---
+++ testimonials-grid-section/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV == "production",
+    content: [
+      './public/index.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'moderate-violet': 'hsl(263, 55%, 52%)',
+        'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'very-dark-blackish-blue': 'hsl(219, 29%, 14%)',
+        'light-gray': 'hsl(0, 0%, 81%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+}
